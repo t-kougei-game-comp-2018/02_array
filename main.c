13bd2f0e60ad249a0dca63f37fe28af2e374d25a
oonuki1030 <43439440+oonuki1030@users.noreply.github.com>
2018-09-26 22:59:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     char str[5];
     
     while(fgets(str, sizeof(str), stdin)){
-        if(str<0){printf("%s", str);}
+        if(str<0){printf("%s", -str);}
         else if(0<=str){ str[]=str; }
     }
 
