1c800b533e8fce4b152570a195f668f7633bdaf3
Ytt-yusuke <43527062+Ytt-yusuke@users.noreply.github.com>
2018-09-30 10:58:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
-    int array[100],input,i = 0;
+    int array[100],i = 0;
     
     while(fgets(str, sizeof(str), stdin)){
-        input = atoi(str);
+        int input = atoi(str);
             
         if(input >= 0)
         {
