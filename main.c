bef0465091535dbef2171ae66ac28ccafe5144e1
rui0926 <43432504+rui0926@users.noreply.github.com>
2018-10-01 02:20:18 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,22 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    int array[100];
+    int input = 0;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        printf("%s\n",str);
+        
+        int n = atoi(str);
+            
+          if( n => 0)
+          {
+              array[input++] = n;
+          }
+        else
+        {
+            printf("%s\n",array[input-1];
+        }
     }
 
     return 0;
