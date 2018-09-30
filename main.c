8f76f1922e2e514a166022c715c8c1a1d093d1c6
Nao-ishigaki <43568604+Nao-ishigaki@users.noreply.github.com>
2018-09-30 19:48:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     
     while(fgets(str, sizeof(str), stdin))
     {
-     int n = atoi(str);
+     int n = quantity(str);
 		if (0 <= n) 
         {
 			array[nun++] = n;
