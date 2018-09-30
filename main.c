6e0648c38f5a5a59f152b44808410e9c9d9e3725
Nao-ishigaki <43568604+Nao-ishigaki@users.noreply.github.com>
2018-09-30 19:43:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) 
 {
     char str[5];
     
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    int array[100];
+    int nunber = 0;
+    
+    while(fgets(str, sizeof(str), stdin))
+    {
+     int n = atoi(str);
+		if (0 <= n) 
+        {
+			array[nunber++] = n;
+		}
+		else 
+        {
+			printf("%d\n", array[-n - 1]);
+		}
     }
 
     return 0;
