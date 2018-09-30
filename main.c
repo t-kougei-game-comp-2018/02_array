983a8ae4016f8132185a82c8b97a353e794fdb29
1827010 <43464024+1827010@users.noreply.github.com>
2018-09-30 21:56:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    int i=0,numebr[100],x;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        x=atoi(str);//整数変換
+        if(0<=x && x<=100 )
+        {
+            number[i]=x;
+            i++; //0番目の考慮
+        }
+        else if (0>=x && x>=-100)
+        {
+           printf("%d\n", number[-x-1]); //-1は0から数え始めるため、1つずらすため
+        }
+        else{continue;}
     }
 
     return 0;
