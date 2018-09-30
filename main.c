8a280fb5fa772dde98fa325cc55a408dd024e83d
Ytt-yusuke <43527062+Ytt-yusuke@users.noreply.github.com>
2018-09-30 10:21:35 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,19 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    int array[100],input,i = 0;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        input = atoi(str);
+            
+        if(input >= 0)
+        {
+            array[i] = input;
+        }
+        else
+        {
+        printf("%s", str[-input]);
+        }
     }
 
     return 0;
