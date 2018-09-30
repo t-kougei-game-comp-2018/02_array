f78331289ffd4541ba44b419c8826e33f729b8fb
Nao-ishigaki <43568604+Nao-ishigaki@users.noreply.github.com>
2018-09-30 19:44:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,14 @@ int main(int argc, char *argv[])
     char str[5];
     
     int array[100];
-    int nunber = 0;
+    int nun = 0;
     
     while(fgets(str, sizeof(str), stdin))
     {
      int n = atoi(str);
 		if (0 <= n) 
         {
-			array[nunber++] = n;
+			array[nun++] = n;
 		}
 		else 
         {
