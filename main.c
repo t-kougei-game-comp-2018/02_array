050e5d633129df5ad1133533fcfbae535c687940
サトウ　ソウマ <38420480+sato-soma@users.noreply.github.com>
2018-10-01 06:59:29 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) 
+
 {
     char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+    int soma[100];
+    int index = 0;
 
+    while(fgets(str, sizeof(str), stdin))
+    {
+        int n = atoi(str);
+        if(0 <= n)
+        {
+            soma[index++] = n;
+        }else
+        {
+            printf("%d\n",soma[-n-1]);
+        }
+    }
     return 0;
 }
