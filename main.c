1b5c6f36b15da8b517ecba0c0d7a7d25e74fd2cf
i-takabe <38421033+i-takabe@users.noreply.github.com>
2018-10-01 10:52:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,25 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[5];
+    char str[256], stdout[256];
+    int in;
+    FILE *stdin;      
+   
+    stdin = fopen("input1.txt","r");     
     
-    while(fgets(str, sizeof(str), stdin)){
+    while(fgets(str, sizeof(str), stdin) != NULL){
+        
         printf("%s", str);
     }
+    for(var i = 0; i < sizeof(str), i++){
+     
+        if(str[i] >= 0){
+            stdout[i] = str[i];
+        }
+        else
+            printf("%d", stdout);
+    }
+    //途中
 
     return 0;
 }
