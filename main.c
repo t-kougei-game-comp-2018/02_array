543785574399a41a005433f1711e2e4ad61c1c2a
i-takabe <38421033+i-takabe@users.noreply.github.com>
2018-11-04 17:18:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    int j = 0;
-    int i = 0;
-    int in;
-    char str[5], stdout[101][5];
-    FILE *stdin;      
-   
-    stdin = fopen("input1.txt","r"); 
-    if(stdin == NULL){
-        printf("ファイル読み込みに失敗しました。\n");
-        
-        return -1;
-    }
-    
-    while(fgets(str, sizeof(str), stdin) != NULL){    
-     
-        if(str[0] >= 0)
-        {
-            for(int i = 0; i < 5; i++){
-                stdout[j][i] = str[i];
-                j++; 
-            }
-        }     
-         else {
-             for(int i = 0; i < 5; i++){
-                printf("%c", stdout[str[i]][i]);
-             }
-        }
-    }
-   
+	int i = 0;
+	int input;
+	char str[5];
+	int stdoutput[101];
+	FILE *stdinput;
 
-    fclose(stdin);
-    
-    return 0;
+	stdinput = fopen("input1.txt", "r");
+	if (stdinput == NULL) {
+		printf("ファイル読み込みに失敗しました。\n");
+
+		return -1;
+	}
+
+	while (fgets(str, sizeof(str), stdinput) != NULL)
+	{
+		input = atoi(str);
+
+		if (input >= 0)
+		{	
+			stdoutput[i] = input;			
+			
+			i++;
+		}
+		else 
+		{
+			printf("%d\n", stdoutput[-1*input-1]);		
+		}
+	}
+
+	fclose(stdinput);
+
+	return 0;
 }
